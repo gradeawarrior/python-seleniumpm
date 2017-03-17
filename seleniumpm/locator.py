e2f63fbb1c9c6d93571612571ca1958a2e0a5472
Peter Salas <psalas@tintri.com>
2017-03-16 18:34:38 -0700

    Adding get_tuple() to Locator object. Also adding simpler way of defining Locators

--- seleniumpm/locator.py
+++ seleniumpm/locator.py
@@ -1,4 +1,5 @@
 import copy
+from selenium.webdriver.common.by import By
 
 class Locator(object):
 
@@ -22,5 +23,43 @@ class Locator(object):
     def __hash__(self):
         return hash(tuple(sorted(self.__dict__.items())))
 
+    def get_tuple(self):
+        """
+        Returns a Locator as a tuple of (by, path)
+        """
+        return (self.by, self.value)
+
     def copy(self):
         return copy.deepcopy(self)
+
+    @staticmethod
+    def by_xpath(value):
+        return Locator(by=By.XPATH, value=value)
+
+    @staticmethod
+    def by_css_selector(value):
+        return Locator(by=By.CSS_SELECTOR, value=value)
+
+    @staticmethod
+    def by_name(value):
+        return Locator(by=By.NAME, value=value)
+
+    @staticmethod
+    def by_class_name(value):
+        return Locator(by=By.CLASS_NAME, value=value)
+
+    @staticmethod
+    def by_id(value):
+        return Locator(by=By.ID, value=value)
+
+    @staticmethod
+    def by_link_text(value):
+        return Locator(by=By.LINK_TEXT, value=value)
+
+    @staticmethod
+    def by_partial_link_text(value):
+        return Locator(by=By.PARTIAL_LINK_TEXT, value=value)
+
+    @staticmethod
+    def by_tag_name(value):
+        return Locator(by=By.TAG_NAME, value=value)
