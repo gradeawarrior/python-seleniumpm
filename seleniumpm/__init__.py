67808e8de23e300f4c5a678b0e520a2a6766d28d
Peter Salas <psalas@tintri.com>
2017-03-27 10:55:51 -0700

    Upreving to 2.9.0 for release

--- seleniumpm/__init__.py
+++ seleniumpm/__init__.py
@@ -9,7 +9,7 @@ Selenium PageModel library
 """
 
 __title__ = 'seleniumpm'
-__version__ = '2.8.0'
+__version__ = '2.9.0'
 __build__ = 0x021000
 __author__ = 'Peter Salas'
 __license__ = 'Apache 2.0'
