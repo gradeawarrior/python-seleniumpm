629bf27eefc98bf86dc9db3c4097b6355ac610a9
Peter Salas <psalas@tintri.com>
2017-04-04 10:12:12 -0700

    Changing seleniumpm.config.reset() to seleniumpm.config.reset_timeouts()

--- seleniumpm/config.py
+++ seleniumpm/config.py
@@ -1,7 +1,7 @@
 page_timeout_in_sec = 30
 element_timeout_in_sec = 10
 
-def reset():
+def reset_timeouts():
     global page_timeout_in_sec
     global element_timeout_in_sec
     page_timeout_in_sec = 30
--- tests/test_seleniumpm_config.py
+++ tests/test_seleniumpm_config.py
@@ -19,63 +19,63 @@ class TestSeleniumpmConfig(object):
             self.driver.quit()
 
     def test_element_defaults(self):
-        seleniumconfig.reset()
+        seleniumconfig.reset_timeouts()
         element = Element(self.driver, Locator.by_xpath("//foo"))
         assert element.page_timeout == 30, "Expecting page_timeout=30"
         assert element.element_timeout == 10, "Expecting element_timeout=10"
 
     def test_element_non_page_timeout_default(self):
-        seleniumconfig.reset()
+        seleniumconfig.reset_timeouts()
         seleniumconfig.page_timeout_in_sec = 15
         element = Element(self.driver, Locator.by_xpath("//foo"))
         assert element.page_timeout == 15, "Expecting page_timeout=15"
         assert element.element_timeout == 10, "Expecting element_timeout=10"
 
     def test_element_non_element_timeout_default(self):
-        seleniumconfig.reset()
+        seleniumconfig.reset_timeouts()
         seleniumconfig.element_timeout_in_sec = 20
         element = Element(self.driver, Locator.by_xpath("//foo"))
         assert element.page_timeout == 30, "Expecting page_timeout=30"
         assert element.element_timeout == 20, "Expecting element_timeout=20"
 
     def test_element_change_defaults_and_then_reset(self):
-        seleniumconfig.reset()
+        seleniumconfig.reset_timeouts()
         seleniumconfig.page_timeout_in_sec = 5
         seleniumconfig.element_timeout_in_sec = 25
         element = Element(self.driver, Locator.by_xpath("//foo"))
         assert element.page_timeout == 5, "Expecting page_timeout=5"
         assert element.element_timeout == 25, "Expecting element_timeout=25"
-        seleniumconfig.reset()
+        seleniumconfig.reset_timeouts()
         assert element.page_timeout == 30, "Expecting page_timeout=30"
         assert element.element_timeout == 10, "Expecting element_timeout=10"
 
     def test_page_defaults(self):
-        seleniumconfig.reset()
+        seleniumconfig.reset_timeouts()
         page = Webpage(self.driver)
         assert page.page_timeout == 30, "Expecting page_timeout=30"
         assert page.element_timeout == 10, "Expecting element_timeout=10"
 
     def test_page_non_page_timeout_default(self):
-        seleniumconfig.reset()
+        seleniumconfig.reset_timeouts()
         seleniumconfig.page_timeout_in_sec = 15
         page = Webpage(self.driver)
         assert page.page_timeout == 15, "Expecting page_timeout=15"
         assert page.element_timeout == 10, "Expecting element_timeout=10"
 
     def test_page_non_element_timeout_default(self):
-        seleniumconfig.reset()
+        seleniumconfig.reset_timeouts()
         seleniumconfig.element_timeout_in_sec = 20
         page = Webpage(self.driver)
         assert page.page_timeout == 30, "Expecting page_timeout=30"
         assert page.element_timeout == 20, "Expecting element_timeout=20"
 
     def test_page_change_defaults_and_then_reset(self):
-        seleniumconfig.reset()
+        seleniumconfig.reset_timeouts()
         seleniumconfig.page_timeout_in_sec = 5
         seleniumconfig.element_timeout_in_sec = 25
         page = Webpage(self.driver)
         assert page.page_timeout == 5, "Expecting page_timeout=5"
         assert page.element_timeout == 25, "Expecting element_timeout=25"
-        seleniumconfig.reset()
+        seleniumconfig.reset_timeouts()
         assert page.page_timeout == 30, "Expecting page_timeout=30"
         assert page.element_timeout == 10, "Expecting element_timeout=10"
