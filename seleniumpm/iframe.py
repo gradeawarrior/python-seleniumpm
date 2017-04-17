4c2add20864489e3c0db814fef07ebfa5bcd2231
Peter Salas <psalas@tintri.com>
2017-04-17 14:05:40 -0700

    Adding try/finally block to catch potential issues when trying to validate() an IFrame

--- seleniumpm/iframe.py
+++ seleniumpm/iframe.py
@@ -1,4 +1,5 @@
 from seleniumpm.webelements.panel import Panel
+from seleniumpm.webelements.element import take_screenshot_on_element_error
 
 
 class IFrame(Panel):
@@ -7,10 +8,13 @@ class IFrame(Panel):
 
     def validate(self, timeout=None, force_check_visibility=False):
         timeout = timeout if timeout is not None else self.element_timeout
-        self.switch_in()
-        super(IFrame, self).validate(timeout=timeout, force_check_visibility=force_check_visibility)
-        self.switch_out()
+        try:
+            self.switch_in()
+            super(IFrame, self).validate(timeout=timeout, force_check_visibility=force_check_visibility)
+        finally:
+            self.switch_out()
 
+    @take_screenshot_on_element_error
     def switch_in(self):
         """
         This is to support switching to an iframe for smart validations of all Element's on a page
@@ -19,9 +23,11 @@ class IFrame(Panel):
 
         :return: self
         """
-        self.driver.switch_to.frame(self.driver.find_element(by=self.locator.by, value=self.locator.value))
+        if self.locator:
+            self.driver.switch_to.frame(self.driver.find_element(by=self.locator.by, value=self.locator.value))
         return self
 
+    @take_screenshot_on_element_error
     def switch_out(self):
         """
         This is to support switching out of an iframe for smart validations of all Element's on a page
